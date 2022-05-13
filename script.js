e029ddf544b2ed1fed08d3f3ee5debda5e977a0a
ChiaraInMiniatura <chiarag295@gmail.com>
2022-05-13 16:03:26 +0200

    modifiche post-correzione

--- script.js
+++ script.js
@@ -10,16 +10,27 @@ const app = new Vue({
 
   data:{
     listaMail: [],
+    isLoading: true,
 
   },
 
   methods:{
     generaMail(){
-      for (let i = 0; i < 10; i++) {;
+      for (let i = 0; i < 10; i++) {
+
         axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+
         .then ((res) =>{
         console.log(res.data.response);
         this.listaMail.push(res.data.response);
+
+        if(this.listaMail.length === 10){
+          this.isLoading = false;
+          console.log("fine");
+        }
+        })
+        .catch((error) =>{
+          console.log(error);
         })
       }
     },
